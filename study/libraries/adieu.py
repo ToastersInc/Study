da44093d25326bc1243b8df7cac70e8a543291c9
ToastersInc <benjac77@gmail.com>
2025-04-02 10:23:32 -0400

    finished adieu.py but didnt do it with the inflect module so its pretty raw

--- study/libraries/adieu.py
+++ study/libraries/adieu.py
@@ -1,5 +1,5 @@
-# implement a program that prompts the user for names, 
-# one per line, until the user inputs control-d. 
+# implement a program that prompts the user for names,
+# one per line, until the user inputs control-d.
 # Assume that the user will input at least one name. Then bid adieu to those names, separating two names with one and,
 # three names with two commas and one and, and names with commas and one and, as in the below:
 
@@ -11,6 +11,32 @@
 #   Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, and Marta
 #   Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, Marta, and Gretl
 
+
 def main():
+    # get names for user one at a time until user presses 'Ctrl + D'
+    names = []
+    while True:
+        try:
+            user_input = input("Name: ").title()
+            names.append(user_input)
+        except EOFError:
+            break
+    print("")
+    if len(names) < 1:
+        print("you didnt enter any names")
+    elif len(names) == 1:
+        print(f"Adieu, adieu, to {names[0]}")
+    elif len(names) == 2:
+        print(f"Adieu, adieu, to {names[0]} and {names[1]}")
+    elif len(names) > 2:
+        for name in names:
+            if name == names[0]:
+                print(f"Adieu, adieu, to {name}, ", end="")
+            elif names.index(name) > 0 and names.index(name) < len(names) - 1:
+                print(f"{name}, ", end="")
+            else:
+                print(f"and {name}")
 
 
+if __name__ == "__main__":
+    main()
